3ed8c0f14c0c9656b43718bafecac45f49d76bb4
mishu <mishu@xtelecom.ro>
2021-06-26 13:47:50 +0300

    improves layout

--- src/bulma/pages/controlPanel/Index.vue
+++ src/bulma/pages/controlPanel/Index.vue
@@ -1,67 +1,58 @@
 <template>
     <div class="wrapper">
-        <top :summary="summary"
-            @update="dateInterval = $event"
-            @refresh="refresh"/>
-        <div class="columns is-multiline">
-            <div class="column is-one-third is-half-tablet"
-                v-for="app in apps"
-                :key="app.id">
-                <application class="raises-on-hover"
-                    :app="app"
-                    :date-interval="dateInterval"
-                    @loaded="updateStats"
-                    ref="apps"/>
+        <div class="columns is-centered">
+            <div class="column is-narrow">
+                <enso-date-filter class="box raises-on-hover"
+                    compact
+                    v-on="$listeners"/>
+            </div>
+        </div>
+        <div class="columns is-centered">
+            <div class="column is-narrow">
+                <indicator :label="i18n('Logins')"
+                    :value="summary.logins"/>
+            </div>
+            <div class="column is-narrow">
+                <indicator :label="i18n('Requests')"
+                    :value="summary.requests"/>
+            </div>
+            <div class="column is-narrow">
+                <div class="box indicator raises-on-hover">
+                    <button class="button is-small is-naked"
+                        @click="$emit('refresh')">
+                        <span class="icon is-small">
+                            <fa icon="sync"/>
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <div class="column is-narrow">
+                <indicator :label="i18n('Users')"
+                    :value="summary.newUsers"/>
+            </div>
+            <div class="column is-narrow">
+                <indicator :label="i18n('Sessions')"
+                    :value="summary.sessions"/>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import Top from './components/Top.vue';
-import Application from './components/Application.vue';
+import { EnsoDateFilter } from '@enso-ui/filters/bulma';
+import Indicator from './Indicator.vue';
 
 export default {
-    name: 'Index',
+    name: 'Top',
 
-    components: { Top, Application },
+    components: { Indicator, EnsoDateFilter },
 
-    inject: ['route'],
+    inject: ['i18n'],
 
-    data: () => ({
-        apps: [],
-        dateInterval: {
-            min: null,
-            max: null,
-        },
+    props: {
         summary: {
-            logins: 0,
-            requests: 0,
-            newUsers: 0,
-            sessions: 0,
-        },
-    }),
-
-    created() {
-        this.fetch();
-    },
-
-    methods: {
-        fetch() {
-            axios.get(this.route('administration.applications.index'))
-                .then(({ data }) => (this.apps = data))
-                .catch(error => this.handleError(error));
-        },
-        refresh() {
-            this.$refs.apps.forEach(app => app.refresh());
-        },
-        updateStats() {
-            Object.keys(this.summary)
-                .forEach(key => (this.summary[key] = this.sum(key)));
-        },
-        sum(key) {
-            return this.$refs.apps
-                .reduce((sum, app) => (sum + app.$data.summary[key]), 0);
+            type: Object,
+            required: true,
         },
     },
 };
